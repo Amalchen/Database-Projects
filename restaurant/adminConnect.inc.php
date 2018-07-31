932220fab3d7e1e1dd9d09b9e2844411eb4ca892
Amalchen <38335524+Amalchen@users.noreply.github.com>
2018-07-31 23:27:57 +0300

    Add files via upload

--- restaurant/adminConnect.inc.php
+++ restaurant/adminConnect.inc.php
@@ -1,9 +1,10 @@
 <?php
-     $mysqlHost = 'localhost';
-     $mysqlUser = 'adminoras';
-     $mysqlPassword = '';
-     $dB = 'restaurant';
-     $conn = mysqli_connect($mysqlHost, $mysqlUser, $mysqlPassword, $dB);
-     if(!$conn)
-          die('Oops, there was in error in our part. Please contact programmer.');
+    $mysqlHost = 'localhost';
+    $mysqlUser = 'adminoras';
+    $mysqlPassword = '';
+    $dB = 'restaurant';
+    $conn = mysqli_connect($mysqlHost, $mysqlUser, $mysqlPassword, $dB);
+    if (!$conn) {
+        die('Oops, there was in error in our part. Please contact programmer.');
+    }
 ?>
--- restaurant/connect.inc.php
+++ restaurant/connect.inc.php
@@ -1,9 +1,10 @@
 <?php
-     $mysqlHost = 'localhost';
-     $mysqlUser = 'helot';
-     $mysqlPassword = '';
-     $dB = 'restaurant';
-     $conn = mysqli_connect($mysqlHost, $mysqlUser, $mysqlPassword, $dB);
-     if(!$conn)
-          die('Oops, there was in error in our part. Please contact: admin@restaurant.org');
+    $mysqlHost = 'localhost';
+    $mysqlUser = 'helot';
+    $mysqlPassword = '';
+    $dB = 'restaurant';
+    $conn = mysqli_connect($mysqlHost, $mysqlUser, $mysqlPassword, $dB);
+    if (!$conn) {
+        die('Oops, there was in error in our part. Please contact: admin@restaurant.org');
+    }
 ?>
